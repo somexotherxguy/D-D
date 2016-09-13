d3d9e7cbf98dee96aa1341e8748109c741924dc8
a <smdvw5@mst.edu>
2016-09-13 13:31:00 -0500

    I added some empty files for the web page and a csv file for ease of use. I also updated my stuff on the projectdoc.

---
+++ index.js
@@ -0,0 +1,7 @@
+/*setTimeout(function(){
+  document.body.innerHTML += "tralala"
+  aarg = 'testerdoo'
+  console.log(aarg)
+}, 100)
+*/
+name_list = ['password', 'icon', 'ideal', 'flaw', 'story', 'gender', 'height', 'race', 'alignment', 'class_name', 'fighting_style', 'ideal', 'language', 'armor', 'weapon', 'proficiency_mod', 'ability', 'strength', 'constitution', 'wisdom', 'dexterity', 'intelligence', 'charisma', '', 'spellcasting', 'feat', 'username', 'character_list', 'char_name', 'traits', 'bonds', 'notable_traits', 'description', 'age', 'weight', 'sub_race', 'exp', 'archetype', 'level', 'hp', 'tool_prof', 'weapon_prof', 'tool', 'skills', 'saves', 'equipment', 'money', 'available_spells', 'notes', '']
