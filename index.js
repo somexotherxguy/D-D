ee403b2973d26a675dc2b36bacb0c8dce8a3771d
Roachn8r <dpr6x6@mst.edu>
2016-10-06 14:49:32 -0500

    Fixed Retrieve - Can now import all values

--- index.js
+++ index.js
@@ -5,15 +5,15 @@
 }, 100)
 */
 
-(function() {
-  class_name = document.getElementById("class");
-  class_name.addEventListener("change", function() { class_selected(document.getElementById('class').value);});
-  
-  submit_button = document.getElementById("submit");
-  submit_button.addEventListener("click", function() { update();});
-  
-  load_button = document.getElementById("load");
-  load_button.addEventListener("click", function() { test_query();});
+class_name = document.getElementById("class");
+class_name.addEventListener("change", function() { class_selected(document.getElementById('class').value);});
+
+submit_button = document.getElementById("submit");
+submit_button.addEventListener("click", function() { update();});
+
+load_button = document.getElementById("load");
+load_button.addEventListener("click", function() { test_query();});
+
   
 name_array = [["Field Name", "Variable Name", "Field Type", "Data Type", "Required?"], ["Password", "password", "text \u2013 password", "string", "y"], ["Char_Icon", "icon", "text", "image file (or hyperlink)", "n"], ["Ideals", "ideal", "text area", "string", "n"], ["Flaws", "flaw", "text area", "string", "n"], ["Backstory", "story", "text area", "string", "n"], ["Gender", "gender", "radio button", "char", "y"], ["Height", "height", "number", "int (in mm)", "y"], ["Race", "race", "dropdown", "string", "y"], ["Alignment", "alignment", "dropdown", "string", "y"], ["Class", "class_name", "dropdown", "string", "y"], ["Fighting Style", "fighting_style", "text?", "string", "n"], ["Background", "background", "text area", "string", "n"], ["Languages", "language", "text", "string", "y"], ["Armor", "armor", "subform", "class", "n"], ["Weapon", "weapon", "subform", "class", "n"], ["Proficiency_Modifier", "proficiency_mod", "number", "int", "n"], ["Abilities", "ability", "subform", "class", "y"], ["Strength", "str", "number", "int", "y"], ["Cons", "con", "number", "int", "y"], ["Wisdom", "wis", "number", "int", "y"], ["Dex", "dex", "number", "int", "y"], ["Int", "int", "number", "int", "y"], ["Charisma", "chr", "number", "int", "y"], ["Misc.", "", "", "", ""], ["Spellcasting", "spellcasting", "subform", "class", "n"], ["Feats", "feat", "dropdown", "string", "n"], ["Username", "username", "text", "string", "y"], ["Character List", "character_list", "hyperlink list", "string", "y"], ["Name", "char_name", "text", "string", "y"], ["Personality Traits", "traits", "subform", "", "n"], ["Bonds", "bonds", "text", "string", "n"], ["Traits_of_Note", "notable_traits", "text area", "string", "n"], ["Description", "description", "text area", "string", "n"], ["Age", "age", "number", "int", "n"], ["Weight", "weight", "number", "int", "y"], ["Sub_race", "sub_race", "dropdown", "string", "n"], ["EXP", "exp", "number", "int", "y"], ["Archetype", "archetype", "dropdown", "string", "y"], ["Level", "level", "number", "int", "y"], ["HP", "hp", "number", "int", "y"], ["Tool_Proficiency", "tool_prof", "number", "int", "y"], ["Weapon_Proficiency", "weapon_prof", "number", "int", "y"], ["Tool", "tool", "subform", "class", "n"], ["Skills", "skills", "dropdown", "string", "y"], ["Saves", "saves", "subform", "class", "y"], ["Equipment", "equipment", "subform", "class", "n"], ["Money", "money", "number", "int", "y"], ["Spells_player_is_able_to_use", "available_spells", "dropdown", "string", "y"], ["Notes", "notes", "text area", "string", "n"]]
 
@@ -194,9 +194,9 @@ function test_query() {
 			document.getElementById("char_info").innerHTML = this.responseText;
 		}
 	};*/
-	json_test = JSON.stringify({"ideal":3,"flaw":2,"story":"Tragic","gender":"male","height":12.5,"race":"Man","alignment":"Humans","class_name":"Warrior","fighting_style":"Melee","background":"Red","proficiency_mod":3,"str":12,"con":13,"wis":"16","dex":14,"int":14,"chr":21,"feat":"lots","char_name":"Trogdor","traits":"Hotheaded","bonds":"Iron","notable_traits":"Dragon-Man","description":"Burninator","age":8972,"weight":37463929,"sub_race":"Dragon-Man","exp":9000,"archetype":"SmackEm","level":69,"hp":1000,"tool_prof":"some","weapon_prof":"fists","skills":"all of them","platinum":123,"gold":500,"electrum":12,"silver":756,"copper":9,"available_spells":"Firebreath","notes":"Remember to buy eggs", "languages":"Python"});
+	json_test = JSON.stringify({"ideal":3,"flaw":2,"story":"Tragic","gender":"male","height":12.5,"race":"Man","alignment":"chaotic neutral","class":"fighter","fighting_style":"dueling","background":"Red","proficiency_mod":3,"str":12,"con":13,"wis":"16","dex":14,"int":14,"chr":21,"feat":"lots","char_name":"Trogdor","traits":"Hotheaded","bonds":"Iron","notable_traits":"Dragon-Man","description":"Burninator","age":8972,"weight":37463929,"sub_race":"Dragon-Man","exp":9000,"archetype":"battle_master","level":"20","hp":1000,"tool_prof":"some","weapon_prof":"fists","skills":"all of them","platinum":123,"gold":500,"electrum":12,"silver":756,"copper":9,"available_spells":"Firebreath","notes":"Remember to buy eggs", "languages":"Python"});
 	r = JSON.parse(json_test);
-	xhttp.open("GET", "object.json", true);
+	xhttp.open("GET", "object.json");
 	xhttp.send();
 	for (k in r) {
 		if (k === "gender") {
@@ -206,12 +206,15 @@ function test_query() {
 		  var row = document.getElementById(k);
 		  if (row) {
 		    row.value = r[k];
+        if (k === "class") {
+          class_selected(row.value);
+        }
 		  }
 		}
 	}
 }
 
-function class_selected(class_name = "fighter")  {
+function class_selected(class_name = "fighter") {
   var options = document.getElementById("class_options");
   options.innerHTML=""
   for (first in class_options[class_name]) break;
@@ -230,20 +233,20 @@ function class_selected(class_name = "fighter")  {
       '<option value = "' + i + '">' + class_options[class_name][first][i]);
   }
   if (last !== first) {
-	options.insertAdjacentHTML("beforeend",
-	'<li><div>Fighting Style<select id="fighting_style"></select></div></li>');
-	var fighting_styles = document.getElementById("fighting_style");
+  	options.insertAdjacentHTML("beforeend",
+  	'<li><div>Fighting Style<select id="fighting_style"></select></div></li>');
+  	var fighting_styles = document.getElementById("fighting_style");
     var length = fighting_styles.options.length;
     for (var i = length - 1; i >= 0; i--) {
       fighting_styles.remove(i);
-	}
-	fighting_styles.insertAdjacentHTML("beforeend",
-	  '<option disabled selected value>' + "-- --" + '</option>');
-	for (var i in class_options[class_name]["fighting_style"]) {
-		  fighting_styles.insertAdjacentHTML("beforeend",
-		  '<option value="' + i + '">' + class_options[class_name]["fighting_style"][i] + '</option>');
-	}
-  }	  
+  	}
+  	fighting_styles.insertAdjacentHTML("beforeend",
+  	  '<option disabled selected value>' + "-- --" + '</option>');
+  	for (var i in class_options[class_name]["fighting_style"]) {
+  		fighting_styles.insertAdjacentHTML("beforeend",
+  		  '<option value="' + i + '">' + class_options[class_name]["fighting_style"][i] + '</option>');
+  	}	  
+  }
 }
 
-})();
+
