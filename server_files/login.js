62cf5de8ae611596f983057fd0bdba71fb950535
ROACH <dpr6x6@mst.edu>
2016-10-24 14:22:16 -0500

    Tried to redirect after login

--- server_files/login.js
+++ server_files/login.js
@@ -2,11 +2,24 @@ users = {
 	"Roach": "burninate"
 };
 
+function redirect(url) {
+	window.location = url;
+}
+
+function change_url(new_url) {
+	var state = {};
+	var title = "Creator";
+	history.pushState(state, title, new_url);
+}
+
 function login() {
 	var username = document.getElementById("username").value;
 	var password = document.getElementById("password").value;
 	if (username in users && users[username] === password) {
-		window.location = "creator.html";
+		//window.location = "creator.html";
+		//redirect("creator.html");
+		//change_url("/user/" + username);
+		redirect("/user/" + username + "/Trogdor/");
 	} else {
 		window.location = "login.html";
 	}
