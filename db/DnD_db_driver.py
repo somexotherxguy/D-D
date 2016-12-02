33c585660fbd94b9e70653d88268b4b594123d75
Rogersl <ljr9z9@mst.edu>
2016-12-02 04:36:20 -0600

    Updated to test string passing (works)

--- db/DnD_db_driver.py
+++ db/DnD_db_driver.py
@@ -6,10 +6,15 @@ import DnD_db_manip
 with open('object.json', 'r') as infile:
 	data = json.load(infile)
 
+print type(data)
+
+data = str(data)
+print type(data)
+
 id_token = "H"
 
 DnD_db_manip.db_char_push(id_token,data)
 
-DnD_db_manip.db_char_pull(id_token, 'Trogdor')
+print DnD_db_manip.db_char_pull(id_token, 'Trogdor')
 
-DnD_db_manip.db_get_char_list(id_token)
+print DnD_db_manip.db_get_char_list(id_token)
